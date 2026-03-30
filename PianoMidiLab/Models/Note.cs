af1a489ffdfe395b045dbba27b522545a1fb64f6
Garth TB | 天卜 <g-art-h@outlook.com>
2026-03-30 17:44:10 +0800

    在Note中存储到下一个同音的距离

--- PianoMidiLab/Models/Note.cs
+++ PianoMidiLab/Models/Note.cs
@@ -3,15 +3,15 @@ namespace PianoMidiLab.Models;
 internal readonly record struct Note(
     uint Start,
     uint Dur,
+    uint MaxDur, // 下一个同音的距离
     byte Pitch,
     byte OnVel,
     byte OffVel,
     byte Ch) {
-    public uint End => Start + Dur;
     public Event NoteOn => new(Start, (byte)(Event.NoteOn | Ch), Pitch, OnVel, null);
 
     public Event EndEvent =>
         OffVel == 0
-            ? new(End, (byte)(Event.NoteOn | Ch), Pitch, 0, null)
-            : new(End, (byte)(Event.NoteOff | Ch), Pitch, OffVel, null);
+            ? new(Start + Dur, (byte)(Event.NoteOn | Ch), Pitch, 0, null)
+            : new(Start + Dur, (byte)(Event.NoteOff | Ch), Pitch, OffVel, null);
 }
